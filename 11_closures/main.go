657d3197b051e9d78070ca572968f8eb50d924b0
Mel Pereira <meldoncharles@hotmail.com>
2021-03-16 00:21:40 -0400

    added variadic arg function call with a closure on variable sum inside it

--- 11_closures/main.go
+++ 11_closures/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func sum(nums...int) int  {
+
+	sum := 0
+	for _, val := range nums{
+		sum += val
+
+	}
+	return sum
+}
+
 func adder() func(int) int {
 	sum := 0
 	return func(x int) int {
@@ -11,6 +21,9 @@ func adder() func(int) int {
 
 }
 func main() {
+
+	n := []int {3,4,5,6}
+	fmt.Println("sum = ",sum(n...))
 	sum := adder()
 	for i := 0; i < 10; i++ {
 		fmt.Println(sum(i))
